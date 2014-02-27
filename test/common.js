ff3f905a0e04528d3808b996cdafde50df003d8b
itayw <itay@joo.la>
2014-02-27 16:16:17 +0200

    #1 progress with datapoints

--- test/common.js
+++ test/common.js
@@ -8,3 +8,8 @@ global._ = require('underscore');
 var chai = require('chai');
 chai.use(require('sinon-chai'));
 global.expect = chai.expect;
+
+global.options = {
+  host: '192.168.0.6',
+  port: 8082
+};
---
+++ test/unit/connectivity.spec.js
@@ -0,0 +1,42 @@
+var kdb = require('../../lib/kairosdb');
+
+describe("connectivity", function () {
+  it("should access kairosdb without problems", function (done) {
+    var client = kdb.init(global.options.host,global.options.port);
+    client.version(function (err, version) {
+      if (err)
+        return done(err);
+      version = version.indexOf('KairosDB');
+      expect(version).to.equal(0);
+      done();
+    });
+  });
+
+  it("should fail accessing kairosdb that does not exist", function (done) {
+    var client = kdb.init();
+    client.version(function (err, version) {
+      if (err)
+        return done();
+      return done(new Error('This is wrong, the previous call should have resulted with error.'));
+    });
+  });
+
+  it("should support specific api versions", function (done) {
+    var client = kdb.init(global.options.host,global.options.port, {apiversion: 'v3'});
+    client.version(function (err, version) {
+      if (err)
+        return done();
+      return done(new Error('This is wrong, the previous call should have resulted with error.'));
+    });
+  });
+
+  it("should output debug info", function (done) {
+    //This is shown in coverage report
+    var client = kdb.init(global.options.host,global.options.port, {debug:true});
+    client.version(function (err, version) {
+      if (err)
+        return done(err);
+      return done();
+    });
+  });
+});
